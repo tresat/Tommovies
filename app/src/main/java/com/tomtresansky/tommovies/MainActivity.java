79b9ecffde2861dc56507d1eeded48cec6f15063
Tom Tresansky <tom.tresansky@gmail.com>
2016-04-06 12:26:59 -0400

    Created project Tommovies.

---
+++ app/src/main/java/com/tomtresansky/tommovies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tomtresansky.tommovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
